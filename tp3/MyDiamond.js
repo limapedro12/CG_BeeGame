96268024bbfe7f3c0086a68126841a1cc03f94be
Pedro Vieira <up202108768@edu.fe.up.pt>
2024-03-05 12:07:29 +0000

    Tangram and UnitCube corrected

--- tp3/MyDiamond.js
+++ tp3/MyDiamond.js
@@ -15,13 +15,19 @@ export class MyDiamond extends CGFobject {
 			-1, 0, 0,	//0
 			0, -1, 0,	//1
 			0, 1, 0,	//2
-			1, 0, 0		//3
+			1, 0, 0,	//3
+			-1, 0, 0,	//4
+			0, -1, 0,	//5
+			0, 1, 0,	//6
+			1, 0, 0,	//7
 		];
 
 		//Counter-clockwise reference of vertices
 		this.indices = [
 			0, 1, 2,
-			1, 3, 2
+			1, 3, 2,
+			5, 4, 6,
+			6, 7, 5
 		];
 
 		this.normals = [
@@ -29,6 +35,11 @@ export class MyDiamond extends CGFobject {
 			0, 0, 1,	//1
 			0, 0, 1,	//2
 			0, 0, 1,	//3
+
+			0, 0, -1,
+			0, 0, -1,
+			0, 0, -1,
+			0, 0, -1,
 		]
 
 		//The defined indices (and corresponding vertices)
--- tp3/MyTriangleBig.js
+++ tp3/MyTriangleBig.js
@@ -15,17 +15,25 @@ export class MyTriangleBig extends CGFobject {
 			-2, 0, 0,	//0
 			2, 0, 0,	//1
 			0, 2, 0,	//2
+			-2, 0, 0,	//3
+			2, 0, 0,	//4
+			0, 2, 0,	//5
 		];
 
 		//Counter-clockwise reference of vertices
 		this.indices = [
 			0, 1, 2,
+			5, 4, 3
 		];
 
 		this.normals = [
 			0, 0, 1,	//0
 			0, 0, 1,	//1
 			0, 0, 1,	//2
+
+			0, 0, -1,
+			0, 0, -1,
+			0, 0, -1
 		]
 
 		//The defined indices (and corresponding vertices)
