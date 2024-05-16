5f2e1ac09acd12aa81c54a0e8900fce3f6d69c2a
Pedro Lima <up202108806@edu.fe.up.pt>
2024-05-16 10:36:06 +0100

    Removing Angle from grass Part 1

--- project/MyGrass.js
+++ project/MyGrass.js
@@ -12,15 +12,6 @@ export class MyGrass extends CGFobject {
 		this.h = 2;
         this.numTriangles = 10;
 
-        // this.angles = [Math.PI/4, Math.PI/4, Math.PI/4, Math.PI/4, Math.PI/4, Math.PI/4, Math.PI/4, Math.PI/4, Math.PI/4];
-        // this.angles = [Math.PI, Math.PI, Math.PI, Math.PI, Math.PI, Math.PI, Math.PI, Math.PI, Math.PI];
-        this.angles = [Math.PI/2, Math.PI/2, Math.PI/2, Math.PI/2, Math.PI/2, Math.PI/2, Math.PI/2, Math.PI/2, Math.PI/2];
-
-        // this.angles = [];
-        // for(var i = 1; i < this.numTriangles+1; i++) {
-        //     this.angles.push(Math.random() * Math.PI/(4*i) + Math.PI/2 - Math.PI/(8*i));
-        // }
-
 		this.initBuffers();
 	}
 	
@@ -29,7 +20,7 @@ export class MyGrass extends CGFobject {
         var v2 = [0, 0, 0];
         var v3 = [this.h, this.h, 0];
 
-        this.add_triangle(v1, v2, v3, -1);
+        this.add_triangle(v1, v2, v3);
 
         for(var i = 0; i < this.numTriangles; i++) {
 
@@ -37,7 +28,7 @@ export class MyGrass extends CGFobject {
             v2 = v3;
             v3 = this.transform_vertix(v1, i);
 
-            this.add_triangle(v1, v2, v3, i);
+            this.add_triangle(v1, v2, v3);
         }
 	}
 
@@ -45,15 +36,12 @@ export class MyGrass extends CGFobject {
         let signal = -1;
         if(num_vertex % 2 == 0) 
             signal = 1;
-
-        let angle = this.angles[num_vertex];
         let dx = signal*(this.h/this.numTriangles);
-        let dy = Math.sin(angle) * 2*this.h + dx;
-        let dz = Math.cos(angle) * 2*this.h + dx;
-        return [v[0]+dx, v[1] + dy, v[2]+dz];
+        let dy = this.h/this.numTriangles;
+        return [v[0]+dx, v[1] + dy, v[2]];
     }
 
-    add_triangle(v1, v2, v3, num_vertex) {
+    add_triangle(v1, v2, v3) {
         this.vertices.push(v1[0], v1[1], v1[2]);
         this.vertices.push(v2[0], v2[1], v2[2]);
         this.vertices.push(v3[0], v3[1], v3[2]);
@@ -62,22 +50,13 @@ export class MyGrass extends CGFobject {
         this.vertices.push(v2[0], v2[1], v2[2]);
         this.vertices.push(v3[0], v3[1], v3[2]);
 
-        let normal_signal = 1;
-        if(num_vertex % 2 == 0) 
-            normal_signal = -1;
+        this.normals.push(0, 1, 1);
+        this.normals.push(0, 1, 1);
+        this.normals.push(0, 1, 1);
 
-        let angle = Math.PI/2;
-        if(num_vertex != -1)
-            angle = this.angles[num_vertex];
-        
-
-        this.normals.push(0, normal_signal*Math.sin(angle - Math.PI/2), normal_signal*Math.cos(angle - Math.PI/2));
-        this.normals.push(0, normal_signal*Math.sin(angle - Math.PI/2), normal_signal*Math.cos(angle - Math.PI/2));
-        this.normals.push(0, normal_signal*Math.sin(angle - Math.PI/2), normal_signal*Math.cos(angle - Math.PI/2));
-
-        this.normals.push(0, -normal_signal*Math.sin(angle - Math.PI/2), -normal_signal*Math.cos(angle - Math.PI/2));
-        this.normals.push(0, -normal_signal*Math.sin(angle - Math.PI/2), -normal_signal*Math.cos(angle - Math.PI/2));
-        this.normals.push(0, -normal_signal*Math.sin(angle - Math.PI/2), -normal_signal*Math.cos(angle - Math.PI/2));
+        this.normals.push(0, -1, -1);
+        this.normals.push(0, -1, -1);
+        this.normals.push(0, -1, -1);
 
         this.indices.push(this.vertices.length/3 - 3, this.vertices.length/3 - 2, this.vertices.length/3 - 1);
         this.indices.push(this.vertices.length/3 - 4, this.vertices.length/3 - 5, this.vertices.length/3 - 6);
@@ -90,8 +69,6 @@ export class MyGrass extends CGFobject {
 		this.texCoords = [];
 		this.get_vertices();
 
-		//The defined indices (and corresponding vertices)
-		//will be read in groups of three to draw triangles
 		this.primitiveType = this.scene.gl.TRIANGLES;
 
 		this.initGLBuffers();
@@ -100,26 +77,5 @@ export class MyGrass extends CGFobject {
 	updateBuffers() {}
 }
 
-// transform_vertix(v, num_vertex) {
-//         // let midle_point = [old_vertices[0][0] + old_vertices[2][0], old_vertices[0][1] + old_vertices[2][1], old_vertices[0][2] + old_vertices[2][2]];
-//         // let line_vector = [midle_point[0] - old_vertices[0], midle_point[1] - old_vertices[1], midle_point[2] - v[2]];
-
-//         let signal = -1;
-//         if(num_vertex % 2 == 0) 
-//             signal = 1;
-
-//         let angle = this.angles[num_vertex];
-//         let dx = signal*(this.h/this.numTriangles);
-//         let dy = Math.sin(angle) * 2*this.h;
-//         let dz = Math.cos(angle) * 2*this.h;
-
-//         // if(line_vector[0] < line_vector[1] && line_vector[0] < line_vector[2]) {
-//         return [v[0]+dx, v[1]+dy, v[2]+dz];
-//         // } else if(line_vector[1] < line_vector[0] && line_vector[1] < line_vector[2]) {
-//         //     return [v[0]+dy, v[1]+dx, v[2]+dz];
-//         // } else {
-//         //     return [v[0]+dz, v[1]+dy, v[2]+dx];
-//         // }
-//     }
 
 
